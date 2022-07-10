6beb4cfd77ca708fa2c70e2db91c0cfb3dc16367
紫幽 <28271655@qq.com>
2022-07-10 11:01:43 +0800

    update

---
+++ Translator/Update.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _ReplaceString_.Translator
+{
+    internal static class Update
+    {
+        public static StringBuilder CacheInfo = new StringBuilder(10000);
+        public static void UpdateTree(TreeNode oldRoot, TreeNode newRoot, TreeNode translation)
+        {
+            Dictionary<string, bool> old;
+            if (oldRoot.children.FirstOrDefault() is not Leaf)
+            {
+                old = new Dictionary<string, bool>(
+                    oldRoot.children.Select(c => new KeyValuePair<string, bool>(c.name, false))
+                    );
+                foreach (TreeNode child in newRoot.children)
+                {
+                    if (old.ContainsKey(child.name))
+                    {
+                        old[child.name] = true;
+                        UpdateTree(oldRoot[child.name], child, translation[child.name]);
+                    }
+                    else
+                    {
+                        CacheInfo.AppendLine($"New Node : {child.name}");
+                    }
+                }
+                foreach(var key in old.Where(pair => !pair.Value).Select(pair => pair.Key))
+                {
+                    CacheInfo.AppendLine($"Missing Node : {key}");
+                }
+            }
+            else
+            {
+                var oldLeafs = oldRoot.children.Cast<Leaf>().ToList();
+                old = new Dictionary<string, bool>(oldLeafs.Select(c => new KeyValuePair<string, bool>(c.value, false)));
+                
+                foreach (Leaf child in newRoot.children.Cast<Leaf>())
+                {
+                    if (old.ContainsKey(child.value))
+                    {
+                        old[child.value] = true;
+                        string temp = (translation[oldLeafs.Where(pair => pair.value == child.value).First().name] as Leaf).value;
+                        oldLeafs.RemoveAt(oldLeafs.FindIndex(pair => pair.value == child.value));
+                        child.value = temp;
+                    }
+                    else
+                    {
+                        CacheInfo.AppendLine($"New Value : {child.name}");
+                    }
+                }
+
+                foreach (var key in old.Where(pair => !pair.Value).Select(pair => pair.Key))
+                {
+                    CacheInfo.AppendLine($"Missing Value : {key}");
+                }
+            }
+        }
+
+    }
+}
